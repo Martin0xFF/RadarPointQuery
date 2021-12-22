6bd3c95a119efbd02c9a72ab520a4bd3537f4078
martin <martin0xff@gmail.com>
2021-12-22 14:21:59 -0500

    created explicit projection function

--- nuscenes_rpq.py
+++ nuscenes_rpq.py
@@ -1,32 +1,30 @@
 import numpy as np
 import cv2
+import sys
 
 from scipy.spatial.transform import Rotation as Rot
 from nuscenes.nuscenes import NuScenes
 from nuscenes.utils.data_classes import RadarPointCloud
 
-def crude_bgr(values):
-    minimum, maximum = np.min(values), np.max(values)
-    ratio = 2*(values-minimum)/(maximum - minimum)
-    b = np.maximum(0, 255*(1-ratio)).astype(int)
-    r = np.maximum(0, 255*(ratio-1)).astype(int)
-    g = 255 - b - r
-    return np.vstack([b, g, r])
-
+from radar_viz import project_radarpoints_onto_img
 
 # Scipy uses quaterions of the form [x, y, z, w]
 # Nuscenes returns quaterions of the form [w, x, y, z]
-
-RadarPointCloud.disable_filters()
-
 nusc = NuScenes(version='v1.0-mini', dataroot="data", verbose=True)
-city_scene = nusc.scene[8]
+city_scene = nusc.scene[int(sys.argv[1])]
 fs_token = city_scene['first_sample_token']
 current_sample = nusc.get('sample', fs_token)
 
 radar_type = "RADAR_FRONT"
 cam_type = "CAM_FRONT"
+no_filter = False
+plot = False
 
+if no_filter:
+    RadarPointCloud.disable_filters()
+
+write_dir = "imgs/"
+counter = 1
 while current_sample is not None:
     # Renderer examples below
     # nusc.render_sample(current_sample['token'])
@@ -37,15 +35,19 @@ while current_sample is not None:
     radar_points = radar_cloud.points
     radar_xyz = radar_points[0:3, :]
 
+    # Radar extrinsics not that we do not need to invert rotation or pose
     radar_calibrated_sensor = nusc.get('calibrated_sensor', radar_sample_data['calibrated_sensor_token'])
     t_car_radar = np.array(radar_calibrated_sensor['translation'])
     q = radar_calibrated_sensor['rotation']
     q = q[1:] + q[0:1] # move w to the back
     C_car_radar = Rot.from_quat(q).as_matrix() # n.b. rotations are represented as quaterions
+    T_car_radar = np.vstack([np.hstack([C_car_radar, t_car_radar[:,None]]), [[0,0,0,1]]])
 
+    # Camera image data
     cam_sample_data = nusc.get('sample_data', current_sample['data'][cam_type])
-    cam_image = cv2.imread("data/" + cam_sample_data['filename'])
+    cam_img = cv2.imread("data/" + cam_sample_data['filename'])
 
+    # Camera extrinsics matrix, note that we invert the rotation and pose
     cam_calibrated_sensor = nusc.get('calibrated_sensor', cam_sample_data['calibrated_sensor_token'])
     t_car_cam = np.array(cam_calibrated_sensor['translation'])
     q = cam_calibrated_sensor['rotation']
@@ -55,36 +57,18 @@ while current_sample is not None:
     t_cam_car = - C_car_cam.T@t_car_cam
     C_cam_car = C_car_cam.T
 
+    T_cam_car = np.vstack([np.hstack([C_cam_car, t_cam_car[:,None]]), [[0,0,0,1]]])
+
+    T_cam_radar = T_cam_car@T_car_radar
     K = np.array(cam_calibrated_sensor['camera_intrinsic'])
 
-    P_ego = t_car_radar[:, None] + C_car_radar@radar_xyz
-    P_car = t_cam_car[:, None] + C_cam_car@P_ego
-    P_img = K@P_car
-    P_img = P_img/P_img[2,:]
+    project_radarpoints_onto_img(cam_img, radar_xyz, radar_points[5,:], radar_points[8:10, :], K, T_cam_radar)
+    if plot:
+        cv2.imshow(cam_type, cam_img)
+        cv2.waitKey(0)
 
-    speeds = np.sqrt(np.square(radar_points[8, :]) + np.square(radar_points[9, :]))
-    cross = radar_points[5, :]
-
-    ranges = np.sqrt(np.square(radar_points[0, :]) + np.square(radar_points[1, :]))
-    bgr = crude_bgr(speeds)
-
-    def weight_from_range(ranges, min_circle=2, max_circle=20):
-        # make closer circles are bigger
-        ranges = 1/(ranges + 1)
-        minimum, maximum = np.min(ranges), np.max(ranges)
-        ratio = (ranges-minimum)/(maximum - minimum)
-        return (max_circle - min_circle)*ratio + min_circle
-
-    sizes = weight_from_range(ranges).astype(int)
-
-    for index, point in enumerate(P_img[0:2].T):
-        # 8 - vx_comp, 9 - vy_comp
-        cv2.circle(cam_image, point.astype(int), sizes[index], bgr[:, index].tolist())
-
-    cv2.imshow(cam_type, cam_image)
-    cv2.waitKey(0)
-
-
+    cv2.imwrite(write_dir + f"{counter:04d}.png", cam_img)
+    counter += 1
     if current_sample['next'] != '':
         current_sample = nusc.get('sample', current_sample['next'])
     else:
--- radar_viz.py
+++ radar_viz.py
@@ -24,11 +24,11 @@ def project_radarpoints_onto_img(img, points, radar_cross, velocity, K, T):
         the width (r,c)
     points - ndarray with shape [:3, :n], n is the number of points
         the rows follow x,y,z (i.e. row 0 represents all x coords of
-        points). The points are in the radar frame    
-    radar_cross - column vector [n,1] of the radar cross-sections,
+        points). The points are in the radar frame
+    radar_cross - column vector [:1,n] of the radar cross-sections,
         these should correspond to the points, i.e. point 0 has
         position x,y,z and radar_cross section rc
-    velocity - [n,2], these velocities correspond to
+    velocity - [:2, :n], these velocities correspond to
         the points similarly to radar crossections. The first column
         is the velocity in the x direction and the second is the
         velocity in the y direction
@@ -39,15 +39,13 @@ def project_radarpoints_onto_img(img, points, radar_cross, velocity, K, T):
     '''
 
     K_aug = np.vstack([np.hstack([K, np.zeros((3,1))]), [0,0,0,1]])
-    points_aug = np.vstack([points, np.ones((1, points.shape[2]))])
+    points_aug = np.vstack([points, np.ones((1, points.shape[1]))])
     points_cam = T@points_aug
     pixel_points = (K_aug@points_cam / points_cam[2,:])[:2, :]
-    
+
 
     bgr = colour_bgr(np.sqrt(np.square(velocity[0, :]) + np.square(velocity[1, :])))
-    sizes = weight_from_range(np.sqrt(np.square(points[0, :]) + np.square(points[1, :])))
-
+    sizes = weight_from_range(np.sqrt(np.square(points[0, :]) + np.square(points[1, :]))).astype(int)
     for i, point in enumerate(pixel_points.T):
         cv2.circle(img, point.astype(int), sizes[i], bgr[:, i].tolist())
 
-    return img
