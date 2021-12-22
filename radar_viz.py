1f5a94a5eea3692b0d71648ec476585fc28e74c1
Martin <martin.ffrench@mail.utoronto.ca>
2021-12-22 13:34:25 -0500

    added code to project radar points into image plane

--- radar_viz.py
+++ radar_viz.py
@@ -1,29 +1,53 @@
-import open3d as o3d
-import glob
 import numpy as np
-import matplotlib.pyplot as plt
+import cv2
 
-print("Loading Sample Radar Data")
-dirs = glob.glob("RADAR_*/")
-files = []
-for d in dirs:
-    files.append(glob.glob(d+"*"))
-print(dirs)
-for i in range(len(files[0])):
-    clouds = []
-    for j in range(0, len(files)):
-        clouds.append(o3d.io.read_point_cloud(files[j][i]))
-        
-        labels = np.array(clouds[j].cluster_dbscan(eps=0.2, min_points=10))
-        max_label = labels.max()
-        colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
-        colors[labels < 0] = 0
-        clouds[j].colors = o3d.utility.Vector3dVector(colors[:, :3])
-        print(files[j][i]) 
-    o3d.visualization.draw_geometries([clouds[0]],
-                                  zoom=0.3412,
-                                  front=[0.4257, -0.2125, -0.8795],
-                                  lookat=[2.6172, 2.0475, 1.532],
-                                  up=[-0.0694, -0.9768, 0.2024])
+def colour_bgr(values):
+    minimum, maximum = np.min(values), np.max(values)
+    ratio = 2*(values-minimum)/(maximum - minimum)
+    b = np.maximum(0, 255*(1-ratio)).astype(int)
+    r = np.maximum(0, 255*(ratio-1)).astype(int)
+    g = 255 - b - r
+    return np.vstack([b, g, r])
 
-exit(0)
+def weight_from_range(ranges, min_circle=2, max_circle=20):
+    # make closer circles are bigger
+    ranges = 1/(ranges + 1)
+    minimum, maximum = np.min(ranges), np.max(ranges)
+    ratio = (ranges-minimum)/(maximum - minimum)
+    return (max_circle - min_circle)*ratio + min_circle
+
+
+def project_radarpoints_onto_img(img, points, radar_cross, velocity, K, T):
+    '''
+    img - numpy nd array with the rows representing
+        the height of the image and cols representing
+        the width (r,c)
+    points - ndarray with shape [:3, :n], n is the number of points
+        the rows follow x,y,z (i.e. row 0 represents all x coords of
+        points). The points are in the radar frame    
+    radar_cross - column vector [n,1] of the radar cross-sections,
+        these should correspond to the points, i.e. point 0 has
+        position x,y,z and radar_cross section rc
+    velocity - [n,2], these velocities correspond to
+        the points similarly to radar crossections. The first column
+        is the velocity in the x direction and the second is the
+        velocity in the y direction
+    K - camera intrinics matrix 3x3, can be determine in matlab using
+        http://www.vision.caltech.edu/bouguetj/calib_doc/
+    T - extrinsics matrix 4x4, the transform from radar frame to camera frame
+        commonly denoted as T_camera_radar
+    '''
+
+    K_aug = np.vstack([np.hstack([K, np.zeros((3,1))]), [0,0,0,1]])
+    points_aug = np.vstack([points, np.ones((1, points.shape[2]))])
+    points_cam = T@points_aug
+    pixel_points = (K_aug@points_cam / points_cam[2,:])[:2, :]
+    
+
+    bgr = colour_bgr(np.sqrt(np.square(velocity[0, :]) + np.square(velocity[1, :])))
+    sizes = weight_from_range(np.sqrt(np.square(points[0, :]) + np.square(points[1, :])))
+
+    for i, point in enumerate(pixel_points.T):
+        cv2.circle(img, point.astype(int), sizes[i], bgr[:, i].tolist())
+
+    return img
